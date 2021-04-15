5bc0c0a32dfc1a1b62f9ecf0ee8833d8ec49232e
corvinus-tr91pc <peter.ugroczky@stud.uni-corvinus.hu>
2021-04-15 11:45:16 +0200

    Kérdések lekérdéezése az adatbázisból

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,7 +30,7 @@ namespace HajosTeszt
             app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
-            app.UseStaticFiles(); 
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -40,6 +41,11 @@ namespace HajosTeszt
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
