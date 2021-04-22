8d43d7390c685486eedacead60abeadd388ea9f1
corvinus-tr91pc <peter.ugroczky@stud.uni-corvinus.hu>
2021-04-22 13:54:29 +0200

    Kérdések betöltése REST API-val

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,101 +1,10 @@
-﻿var kérdések;
-var sorszám;
-var ksorszám;
-
-function letöltésBefejezedődött(d) {
-    console.log("Sikeres letöltés!")
-    console.log(d)
-    kérdések = d
-    kérdésMegjelenítés(kérdések, 0)
-}
-
-function letöltés() {
-    fetch("questions.json")
-        .then(response => response.json())
-        .then(data => letöltésBefejezedődött(data))
-}
+﻿var sorszam;
+var question;
 
 function szinez() {
-
-}
-
-function kérdésMegjelenítés(kérdés, szám) {
-    var ide = document.getElementById("kérdés_szöveg")
-    ksorszám = szám;
-    ide.innerHTML = kérdés[szám].questionText
-    document.getElementById("válasz1").innerHTML = kérdés[szám].answer1
-    document.getElementById("válasz2").innerHTML = kérdés[szám].answer2
-    document.getElementById("válasz3").innerHTML = kérdés[szám].answer3
-    var válasz1 = document.getElementById("válasz1")
-    var válasz2 = document.getElementById("válasz2")
-    var válasz3 = document.getElementById("válasz3")
-    sorszám = szám;
-
-    document.getElementById("gomb_vissza").addEventListener('click', event => {
-        document.getElementById("válasz1").style.backgroundColor = "lavenderblush"
-        document.getElementById("válasz2").style.backgroundColor = "lavenderblush"
-        document.getElementById("válasz3").style.backgroundColor = "lavenderblush"
-        var k = sorszám - 1
-        var kszám = k % 3;
-        ksorszám = kszám;
-        if (kszám == 0) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-        }
-        if (kszám == 1) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-        }
-        if (kszám == 2) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-        }
-        if (sorszám < 0) {
-            sorszám = 0;
-        }
-        else {
-            sorszám--;
-        }
-    });
-
-    document.getElementById("gomb_előre").addEventListener('click', event => {
-        document.getElementById("válasz1").style.backgroundColor = "lavenderblush"
-        document.getElementById("válasz2").style.backgroundColor = "lavenderblush"
-        document.getElementById("válasz3").style.backgroundColor = "lavenderblush"
-        var k = sorszám + 1
-        var kszám = k % 3;
-        ksorszám = kszám;
-        if (kszám == 0) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-            
-        }
-        if (kszám == 1) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-        }
-        if (kszám == 2) {
-            ide.innerHTML = kérdés[kszám].questionText
-            válasz1.innerHTML = kérdés[kszám].answer1
-            válasz2.innerHTML = kérdés[kszám].answer2
-            válasz3.innerHTML = kérdés[kszám].answer3
-        }
-        sorszám++;
-    });
-
     document.getElementById("válasz1").addEventListener('click', event => {
         var gomb = document.getElementById("válasz1")
-        if (kérdés[ksorszám].correctAnswer == 1) {
+        if (question.correctAnswer == 1) {
             gomb.style.backgroundColor = "green";
         }
         else {
@@ -105,7 +14,7 @@ function kérdésMegjelenítés(kérdés, szám) {
 
     document.getElementById("válasz2").addEventListener('click', event => {
         var gomb = document.getElementById("válasz2")
-        if (kérdés[ksorszám].correctAnswer == 2) {
+        if (question.correctAnswer == 2) {
             gomb.style.backgroundColor = "green";
         }
         else {
@@ -115,18 +24,88 @@ function kérdésMegjelenítés(kérdés, szám) {
 
     document.getElementById("válasz3").addEventListener('click', event => {
         var gomb = document.getElementById("válasz3")
-        if (kérdés[ksorszám].correctAnswer == 3) {
+        if (question.correctAnswer == 3) {
             gomb.style.backgroundColor = "green";
         }
         else {
             gomb.style.backgroundColor = "red"
         }
     });
-    
-}
-    
-window.onload = () => {
-    letöltés()
-    
 }
 
+function visszaszinez() {
+    document.getElementById("válasz1").style.backgroundColor = "lavenderblush"
+    document.getElementById("válasz2").style.backgroundColor = "lavenderblush"
+    document.getElementById("válasz3").style.backgroundColor = "lavenderblush"
+}
+
+
+
+
+
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );
+
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    question = kérdés;
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    if (document.getElementById("kép").src == "") {
+        document.getElementById("kép").hidden = true;
+    }
+    else {
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        document.getElementById("kép").hidden = false;
+    }
+    
+
+
+
+    szinez();
+
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                sorszam = id;
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+
+}
+
+window.onload = () => {
+    kérdésBetöltés(1)
+
+    document.getElementById("gomb_előre").addEventListener('click', event => {
+        visszaszinez();
+        sorszam++;
+        kérdésBetöltés(sorszam);
+    });
+
+
+
+    document.getElementById("gomb_vissza").addEventListener('click', event => {
+        visszaszinez();
+        if (sorszam > 1) {
+            sorszam = sorszam - 1;
+            kérdésBetöltés(sorszam);
+        }
+        else {
+            kérdésBetöltés(1);
+        }
+
+    });
+}
