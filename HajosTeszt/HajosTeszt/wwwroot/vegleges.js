8bcd6191726440d7b91a6c02c9e0d1f14a3423b8
corvinus-tr91pc <peter.ugroczky@stud.uni-corvinus.hu>
2021-05-15 11:29:36 +0200

    Viccek

--- HajosTeszt/HajosTeszt/wwwroot/vegleges.js
+++ HajosTeszt/HajosTeszt/wwwroot/vegleges.js
@@ -30,7 +30,7 @@ function init() {
     if (localStorage.getItem("nextQuestion")) {
         nextQuestion = parseInt(localStorage.getItem("nextQuestion"));
     }
-    if (hotList.length === 0) {
+    if (!localStorage.getItem("hotList")) {
         for (let i = 0; i < questionsInHotList; i++) {
             kérdésBetöltés(nextQuestion, i);
             nextQuestion++;
